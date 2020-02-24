b02d0534bb3f37e1faa190bbef6114f4c877f742
aidinrakanov <altynaikanatbekova@gmail.com>
2020-02-25 00:14:29 +0600

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,7 +15,7 @@ public class Main {
         listA.add(sc.nextLine());
         listA.add(sc.nextLine());
 
-        System.out.println("spisok A " +listA);
+        System.out.println("spisok A " + listA);
 
         listA.ensureCapacity(10);
         ArrayList<String> listB = new ArrayList<>(10);
@@ -26,26 +26,45 @@ public class Main {
         listB.add(sc.nextLine());
         listB.add(sc.nextLine());
 
-        System.out.println("spisok B " +listB);
+        System.out.println("spisok B " + listB);
 
         ArrayList<String> listC = new ArrayList<>();
-       //Collections.sort(listA);
-        //Collections.sort(listB);
-        //Collections.reverse(listB);
+        Collections.reverse(listB);
 
+        Iterator<String> iter = listC.iterator();
 
-        listC.addAll(listA);
-        listC.add(1, listB.get(4));
-        listC.add(3, listB.get(3));
-        listC.add(5, listB.get(2));
-        listC.add(7, listB.get(1));
-        listC.add(9, listB.get(0));
-        System.out.println("spisok C " +listC);
-        Collections.sort(listC);
-
-        listC.toString().length();
-        System.out.println("spisok C po dline " +listC);
+        for (int i = 0; i < listB.size(); i++) {
+            listC.add(listA.get(i));
+            listC.add(listB.get(i));
+            iter = listC.iterator();
+        }
+        while (iter.hasNext()) {
+            String str = iter.next();
 
         }
-    }
+        System.out.println("spisok C2" +listC);
 
+
+        Collections.sort(listC, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+
+        });
+        iter = listC.iterator();
+        while (iter.hasNext()) {
+            String str = iter.next();
+            System.out.println(str);
+        }
+
+    }
+}
+
+//        listC.add(1, listB.get(4));
+//        listC.add(3, listB.get(3));
+//        listC.add(5, listB.get(2));
+//        listC.add(7, listB.get(1));
+//        listC.add(9, listB.get(0));
+//        System.out.println("spisok C " +listC);
+//       Collections.sort(listC);
